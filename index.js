e0de5723464159a0f2928de1285f598950e92a14
John Garbero <john.garbero.itb@gmail.com>
2022-05-26 13:47:12 -0500

    commit tree 6126d3ae632e8499eaa4d86191200ee395e55e40

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Add your code here
+
+function submitData() {
+    return fetch('//localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com"
+        })
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+    .catch(error => {
+        alert("Bad things!")
+        console.log(error.message)
+    })
+}
+
+submitData()
+
+
+
+
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle",
+//     }),
+//   };
+  
+//   function runConfig() {
+//     fetch("http://localhost:3000/dogs", configurationObject);
+//   }
+
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
+  
